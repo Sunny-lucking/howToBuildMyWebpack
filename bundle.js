d902a9530cc195c2b033dbe288137875cc87eb73
培歌超努力 <934778945@qq.com>
2020-07-23 23:25:08 +0800

    八、处理两个关键字

--- bundle.js
+++ bundle.js
@@ -42,6 +42,29 @@ const parseModules = (file) =>{
             code:moduleInfo.code
         }
     })
-    console.log(depsGraph)
+    return depsGraph
 }
-parseModules("./src/index.js")
+const bundle = (file) =>{
+    const depsGraph = JSON.stringify(parseModules(file))
+    return `(function (graph) {
+        function require(file) {
+            function absRequire(relPath) {
+                return require(graph[file].deps[relPath])
+            }
+            var exports = {};
+            (function (require,exports,code) {
+                eval(code)
+            })(absRequire,exports,graph[file].code)
+            return exports
+        }
+        require('${file}')
+    })(${depsGraph})`
+
+}
+const content = bundle('./src/index.js')
+
+console.log(content);
+
+//写入到我们的dist目录下
+fs.mkdirSync('./dist');
+fs.writeFileSync('./dist/bundle.js',content)
